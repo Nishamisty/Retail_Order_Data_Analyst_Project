fdba13431f0332dbb41b3a8662d9533091698711
Nishamisty <nishamisty06@gmail.com>
2025-03-20 16:53:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,145 +7,147 @@ import sqlite3
 
 # Establish database connection
 @st.cache_resource  # Cache connection for performance
-def connect_to_database():
-    try:
-        conn = sqlite3.connect("data.db")
-        cursor = conn.cursor()
-        return conn
-    except Exception as e:
-        st.error("Failed to connect to the database.")
-        st.error(f"Error: {e}")
-        return None
+def load_data():
+    df1 = pd.read_csv(r"order.csv")  
+    df2 = pd.read_csv(r"product.csv")
+    return df1, df2
 
-conn = connect_to_database()
+df1, df2 = load_data()
+
+# Function to execute a query and return the result as a pandas DataFrame
+def run_query(query):
+    conn = sqlite3.connect(":memory:")
+    cursor = conn.cursor()
+    df1.to_sql("order_data", conn, index=False, if_exists="replace")
+    df2.to_sql("product_data", conn, index=False, if_exists="replace")
+    if conn is None:
+        return None  # Return None if connection failed
+
+    try:
+        df_result = pd.read_sql_query(query, conn)
+        return df_result
+    except Exception as e:
+        st.error(f"Error executing query: {e}")
+        return None
+    finally:
+        conn.close()
 
 #Queries
 given_queries ={
-    "1. Top 10 Highest Revenue Generating Products": """
-        SELECT
-            category AS product_name,
-            SUM(quantity * list_price) AS total_revenue
-        FROM orders_csv
-        GROUP BY category
-        ORDER BY total_revenue DESC
-        LIMIT 10;""",
-    "2. Top 5 Cities with Highest Profit Margins": """
-        SELECT city,
-            ROUND(
-               CASE
-               WHEN SUM(quantity * list_price) = 0 THEN 0  -- Avoid division by zero
-               ELSE (SUM(quantity * (list_price - cost_price))::NUMERIC /
-                     NULLIF(SUM(quantity * list_price), 0)) * 100
-           END, 2) AS profit_margin
-        FROM orders_csv WHERE list_price IS NOT NULL AND cost_price IS NOT NULL AND quantity > 0  -- Ensure no missing or invalid data
-        GROUP BY city
-        ORDER BY profit_margin DESC LIMIT 5;""",
-    "3. Calculate the total discount given for each category": """
-        SELECT category,
-               SUM(COALESCE(list_price, 0) * COALESCE(quantity, 0) * (COALESCE(discount_percent, 0)::NUMERIC / 100)) AS total_discount
-        FROM orders_csv
-        GROUP BY category
-        ORDER BY total_discount DESC;""",
-    "4. Find the average sale price per product category":"""
-        SELECT category,
-               ROUND(SUM(list_price * quantity) / SUM(quantity), 2) AS average_sale_price
-        FROM orders_csv
-        GROUP BY category
-        ORDER BY average_sale_price DESC;""",
-    "5. Find the region with the highest average sale price":"""
-        SELECT region,
-               ROUND(SUM(list_price * quantity) / SUM(quantity), 2) AS average_sale_price
-        FROM orders_csv
-        GROUP BY region
-        ORDER BY average_sale_price DESC;""",
-    "6. Find the total profit per category":"""
-        SELECT category,
-               ROUND(SUM((list_price - cost_price) * quantity) ,2) AS total_profit
-        FROM orders_csv
-        GROUP BY category
-        ORDER BY total_profit DESC;""",
-    "7. Identify the top 3 segments with the highest quantity of orders":"""
-        SELECT segment,
-               SUM(quantity) AS total_quantity
-        FROM orders_csv WHERE quantity > 0  -- Ensure no invalid data
-        GROUP BY segment
-        ORDER BY total_quantity DESC LIMIT 3;""",
-    "8. Determine the average discount percentage given per region":"""
-        SELECT region,
-               ROUND(AVG(discount_percent) ,2) AS average_discount_percentage
-        FROM orders_csv
-        GROUP BY region
-        ORDER BY average_discount_percentage DESC;""",
-    "9. Find the product category with the highest total profit":"""
-        SELECT category,
-               ROUND(SUM((list_price - cost_price) * quantity), 2) AS total_profit
-        FROM orders_csv
-        GROUP BY category
-        ORDER BY total_profit DESC LIMIT 1;""",
-    "10.Calculate the total revenue generated per year":"""
-        SELECT EXTRACT(YEAR FROM order_date) AS year,
-               ROUND(SUM(list_price * quantity), 2) AS total_revenue
-        FROM orders_csv
-        GROUP BY year
-        ORDER BY year;"""
-}
+    "Top 10 Products by Revenue": """
+            SELECT p.product_id, p.sub_category, SUM(o.sale_price) AS revenue 
+            FROM product_data p 
+            JOIN order_data o ON p.product_id = o.product_id 
+            GROUP BY p.product_id 
+            ORDER BY revenue DESC 
+            LIMIT 10;
+        """,
+        "Top 5 Cities by Profit Margin": """
+            SELECT city, AVG(
+                CASE WHEN sale_price = 0 THEN 0 ELSE ((profit / sale_price) * 100) END
+            ) AS profit_margin 
+            FROM order_data 
+            GROUP BY city 
+            ORDER BY profit_margin DESC 
+            LIMIT 5;
+        """,
+        "Total Discount by Category": """
+            SELECT p.category, SUM(o.discount_price * o.quantity) AS total_discount 
+            FROM product_data p 
+            JOIN order_data o ON p.product_id = o.product_id 
+            GROUP BY p.category;
+        """,
+        "Average Sale Price by Category": """
+            SELECT p.category, AVG(o.sale_price) AS Avg_saleprice 
+            FROM order_data o 
+            JOIN product_data p ON p.product_id = o.product_id 
+            GROUP BY category;
+        """,
+        "Region with Highest Average Sales": """
+            SELECT region, AVG(sale_price) AS avg_sales 
+            FROM order_data 
+            GROUP BY region 
+            ORDER BY avg_sales DESC 
+            LIMIT 1;
+        """,
+        "Total Profit by Category": """
+            SELECT p.category, SUM(o.profit) AS total_profit 
+            FROM product_data p 
+            JOIN order_data o ON p.product_id = o.product_id 
+            GROUP BY p.category;
+        """,
+        "Highest Quantity Sold by Segment": """
+            SELECT segment, SUM(quantity) AS highest_quantity  
+            FROM order_data 
+            GROUP BY segment 
+            ORDER BY highest_quantity DESC;
+        """,
+        "Average Discount by Region": """
+            SELECT region, ROUND(AVG(discount_percent), 2) AS avg_discount 
+            FROM order_data 
+            GROUP BY region;
+        """,
+        "Most Profitable Category": """
+            SELECT p.category, ROUND(SUM(o.profit), 2) AS total_profit 
+            FROM product_data p 
+            JOIN order_data o ON p.product_id = o.product_id 
+            GROUP BY p.category 
+            ORDER BY total_profit DESC 
+            LIMIT 1;
+        """,
+        "Annual Revenue": """
+            SELECT order_year, ROUND(SUM(sale_price), 2) AS Revenue_per_year 
+            FROM order_data 
+            GROUP BY order_year;
+        """
+    }
 own_queries ={
-    "1. Select All Orders with Product Details":"""
-        SELECT ob.order_id,ob.order_date, ob.region, ob.segment, ob.ship_mode, od.category, od.quantity, od.cost_price, od.list_price
-        FROM orders_basic ob
-        JOIN orders_details od ON ob.order_id = od.order_id;""",
-    "2. Find Orders in a Specific Region":"""
-        SELECT ob.order_id, ob.order_date, ob.region, ob.city, od.category, od.quantity
-        FROM orders_basic ob
-        JOIN orders_details od ON ob.order_id = od.order_id WHERE ob.region = 'West';""",
-    "3. Find All Orders for a Specific Date":"""
-        SELECT ob.order_id, ob.order_date, ob.city, ob.state, od.category, od.quantity
-        FROM orders_basic ob JOIN orders_details od ON ob.order_id = od.order_id WHERE ob.order_date = '2022-06-20';""",
-    "4. Find Total Discount per Order":"""
-        SELECT ob.order_id,
-              SUM(od.quantity * od.list_price * (od.discount_percent / 100)) AS total_discount
-        FROM orders_basic ob
-        JOIN orders_details od ON ob.order_id = od.order_id
-        GROUP BY ob.order_id ORDER BY total_discount DESC;""",
-    "5. Get Orders for a Specific Customer Segment":"""
-        SELECT ob.order_id, ob.order_date, ob.segment, od.category, od.quantity
-        FROM orders_basic ob
-        JOIN orders_details od ON ob.order_id = od.order_id WHERE ob.segment = 'Corporate';""",
-    "6. Calculate the Average List Price of Products":"""
-        SELECT ob.order_id, AVG(od.list_price) AS avg_list_price
-        FROM orders_basic ob
-        JOIN orders_details od ON ob.order_id = od.order_id
-        GROUP BY ob.order_id
-        ORDER BY avg_list_price DESC;""",
-    "7. Find Orders with the Highest Quantity Ordered":"""
-        SELECT ob.order_id, ob.order_date,
-               SUM(od.quantity) AS total_quantity
-        FROM orders_basic ob
-        JOIN orders_details od ON ob.order_id = od.order_id
-        GROUP BY ob.order_id
-        ORDER BY total_quantity DESC LIMIT 10;""",
-    "8. Find Total Revenue Per Order":"""
-        SELECT ob.order_id,
-              SUM(od.quantity * od.list_price) AS total_revenue
-        FROM orders_basic ob
-        JOIN orders_details od ON ob.order_id = od.order_id
-        GROUP BY ob.order_id
-        ORDER BY total_revenue DESC;""",
-    "9. Find the Total Profit for Each Order":"""
-        SELECT ob.order_id,
-              SUM((od.list_price - od.cost_price) * od.quantity) AS total_profit
-        FROM orders_basic ob
-        JOIN orders_details od ON ob.order_id = od.order_id
-        GROUP BY ob.order_id
-        ORDER BY total_profit DESC;""",
-    "10.Get All Orders with Their Total Discount Applied":"""
-        SELECT ob.order_id,
-               SUM(od.quantity * od.list_price * (od.discount_percent / 100)) AS total_discount
-        FROM orders_basic ob
-        JOIN orders_details od ON ob.order_id = od.order_id
-        GROUP BY ob.order_id
-        ORDER BY total_discount DESC;"""
-}
+       "Identify the top 5 states with the highest total sales revenue, grouped by category":"""
+        select o.state, p.product_id,p.category,round(sum(o.sale_price),2) as sale_price
+        from order_data o join product_data p on o.product_id=p.product_id group by state,p.product_id
+        order by sum(sale_price) desc limit 5;
+        """,
+        "Calculate the Total Number of Orders, Quantity, and Revenue for Each Product Category":"""
+        select p.category,sum(o.order_id) as order_count,o.quantity,round(sum(o.sale_price),2)
+        as revenue from product_data p join order_data o on p.product_id=o.product_id group by p.category,o.quantity;
+        """,
+        "Identify the top 10 products with the highest quantities sold, along with their total revenue and profit":"""
+        select p.product_id, p.category, sum(o.quantity) as Total_quantity,
+        round(sum(o.sale_price),2) as Revenue, round(sum(o.profit),2) as profit from 
+        product_data p join order_data o on p.product_id=o.product_id group by 
+        p.product_id order by sum(o.quantity) desc limit 10;
+        """,
+        "Rank all regions by the total quantity of products sold, including the total number of orders":"""
+        select o.region, sum(o.quantity) as total_quantity, count(o.order_id) as order_count,
+        rank() over(order by sum(quantity) desc) from order_data o join 
+        product_data p on p.product_id=o.product_id group by o.region;
+        """,
+        "Determine the Top 3 Customers Based on Their Total Profit Contribution":"""
+        select segment, round(sum(profit),2) as profit, rank() 
+        over(order by sum(profit) desc) from order_data group by segment;
+        """,
+        "Determine the Average Quantity Ordered Per Product":"""
+        select p.sub_category, round(avg(o.quantity),2) as Avg_quantity,
+        count(o.order_id) as total_order from product_data p join order_data o on 
+        o.product_id=p.product_id group by p.sub_category;
+        """,
+        "Analysis of Total Revenue Generated by Each Segment":"""
+        select segment, round(sum(sale_price),2) as total_revenue 
+        from order_Data group by segment;
+        """,
+        "Calculate the total profit for all regions":"""
+        select region, round(sum(profit),2) as Total_profit from order_Data 
+        group by region order by sum(profit) desc;
+        """,
+        "Which state placed the highest quantity of orders, and what was the corresponding shipping mode?":"""
+        select state, sum(quantity) as Total_quanity, ship_mode from order_data 
+        group by state,ship_mode order by sum(quantity) desc;
+        """,
+        "Identify the Month with the Highest Revenue":"""
+        select order_month, round(sum(sale_price),2) as profit,
+        rank() over(order by sum(sale_price) desc) from order_data group 
+        by order_month;"""
+    }
 
 # Streamlit UI
 st.title("Retail Order Data Analysts Project")
